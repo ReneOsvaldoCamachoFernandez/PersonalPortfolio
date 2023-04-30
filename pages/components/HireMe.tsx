b607f6389ce19f75151a96654bb576832da78a70
ReneOsvaldoCamachoFernandez <124537731+ReneOsvaldoCamachoFernandez@users.noreply.github.com>
2023-04-29 22:01:55 -0400

    Initial Commit

--- pages/components/HireMe.tsx
+++ pages/components/HireMe.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const HireMe = () => {
   return (
     <div
-      className="fixed left-4 bottom-4 flex items-center justify-center 
+      className="fixed left-[16px] bottom-4 flex items-center justify-center 
     overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto 
     md:absolute sm:right-0"
     >
